9f40420867f038681edbe6b8c0c3eb5ea526e22d
yip-i <75112256+yip-i@users.noreply.github.com>
2021-11-05 12:28:22 -0700

    Updates.

--- load_data.py
+++ load_data.py
@@ -7,7 +7,11 @@ def main():
     data = pd.read_csv('owid-covid-data.csv')
     print(data.columns)
 
+
     data_top = data.head()
+
+    nan_columns = data.columns[data.isnull().any()]
+    print(nan_columns)
 
 
     print("Hello World!\n")
