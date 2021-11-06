acde60857f0c21a7de23e7e107fd9b6f9a885b7d
yip-i <75112256+yip-i@users.noreply.github.com>
2021-11-06 13:28:55 -0700

    Updates.

--- load_data.py
+++ load_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def main():
 
@@ -13,8 +13,17 @@ def main():
     nan_columns = data.columns[data.isnull().any()]
     print(nan_columns)
 
+    # np.where can be used when there is only a single condition
+    #data['continent'] = np.where(data['continent'].isnull()  , data['location'], data['continent'])
 
-    print("Hello World!\n")
+    #Total cases is mostly likely 0 if it is NULL
+    data['total_cases'] = np.where(data['total_cases'].isnull(), data['total_cases'], 0)
+    data['new_cases'] = np.where(data['new_cases'].isnull(), data['new_cases'], 0)
+
+
+
+    print(data.continent.unique())
+
 main()
 
 
