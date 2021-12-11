4d8db5841f306e993c52e7c48397ca4a2844a09c
Mitchell Lawson <mitchelllawson@Mitchells-MacBook-Pro.local>
2021-12-11 05:59:37 -0800

    added coefficient display and a graph

--- pcr_attempt.py
+++ pcr_attempt.py
@@ -16,19 +16,33 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 #https://scikit-learn.org/stable/auto_examples/cross_decomposition/plot_pcr_vs_pls.html
 def pls_regression(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
     score = []
+    MSE = []
+    coefficients = []
 
     for i in range(1, 40, 1):
         pls = PLSRegression(n_components=i)
         pls.fit(X_train, y_train)
         print(f"I = {i}")
+
         score.append(pls.score(X_test, y_test))
+        y_pred = pls.predict(X_test)
+        MSE.append(mean_squared_error(y_true=y_test, y_pred=y_pred, squared=False))
+
+        coefficients.append([])
+
+        for j in pls.coef_:
+            coefficients[i-1].append(j[0])
+
+        print(f"MSE: {MSE[i // 3]}")
         print(score[i-1])
+        print(pls.coef_)
 
     plt.plot(list(range(1, 40, 1)), score)
     plt.xticks(list(range(0, 41, 2)))
@@ -36,6 +50,30 @@ def pls_regression(X, y):
     plt.ylabel("Score (R^2)?")
     plt.title("Score with varying number of components")
     plt.show()
+
+    plt.plot(list(range(1, 40, 1)), MSE)
+    plt.xticks(list(range(0, 41, 2)))
+    plt.xlabel("Number of components")
+    plt.ylabel("MSE")
+    plt.title("Score with varying number of components")
+    plt.show()
+
+    # So for this part, I actually wanted to see that the pls model performed dimension reduction
+    # but is seems like there's still just as many dimensions. I know that the column headings that I
+    # added don't actually correspond to the data shown but we should at least see more 0's.
+    coefficients = pd.DataFrame(coefficients, columns=X_train.columns)
+    print(coefficients)
+
+    '''
+    plt.plot(list(range(1, 40, 1)), MSE)
+    plt.xticks(list(range(0, 41, 2)))
+    plt.xlabel("Number of components")
+    plt.ylabel("MSE")
+    plt.title("Score with varying number of components")
+    plt.show()
+    '''
+
+
 
 
 def pls_cross_val(X, y):
@@ -81,6 +119,7 @@ def pca(X,y):
 
 def main():
     data = pd.read_csv('owid-covid-data.csv')
+
     uk_data = data.drop(data[data['iso_code'] != "GBR"].index)
     uk_data = uk_data.fillna(0)
     uk_data = uk_data.drop("iso_code", axis = 1)
@@ -94,10 +133,13 @@ def main():
     uk_data['date'] = pd.to_datetime(uk_data['date'])
     uk_data['date'] = uk_data['date'].map(dt.datetime.toordinal)
 
-#Need to drop columns related to new cases.
+    #Need to drop columns related to new cases.
     uk_data = uk_data.drop("new_cases_smoothed", axis = 1)
     uk_data = uk_data.drop("new_cases_per_million", axis = 1)
     uk_data = uk_data.drop("new_cases_smoothed_per_million", axis = 1)
+
+    #min_max_scaler = MinMaxScaler().fit_transform(data)
+    #data = pd.DataFrame(min_max_scaler, columns=data.columns)
 
 
     pls_regression(uk_data, num_cases)
